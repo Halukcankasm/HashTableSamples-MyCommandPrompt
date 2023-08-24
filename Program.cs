80a641adc0691e6073a1efe3af2f00149e3d91f7
halukcan kasım <halukcankasim@hotmail.com>
2023-08-25 00:24:13 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableSamples_MyommandPrompt
+{
+
+    class Islemler
+    {
+        public void EkranTemizlemek()
+        {
+            Console.Clear();
+            Console.WriteLine("============FreeCommandStarted=============");
+            Console.Beep();
+        }
+
+        public void IcerikGostermek(string path)
+        {
+            string[] files = Directory.GetFiles(path);
+
+            foreach (string file in files) 
+            {
+                Console.WriteLine($">> {file}");
+            }
+        }
+
+        public void KullanıcıAdi()
+        {
+            Console.WriteLine("Aktif Kullanıcı:"+Environment.UserName);
+        }
+    }
+
+    public delegate void Islem();
+
+    public delegate void ParametrikIslem(string parametre);
+
+
+    class KomutYorumlayıcı
+    {
+        private Hashtable komutlar = null;
+        private Islemler islemler = null;
+        public KomutYorumlayıcı()
+        { 
+            komutlar = new Hashtable(3);
+
+            islemler = new Islemler();
+
+            komutlar.Add("clr", new Islem(islemler.EkranTemizlemek));
+            komutlar.Add("who", new Islem(islemler.KullanıcıAdi));
+            komutlar.Add("ls", new ParametrikIslem(islemler.IcerikGostermek));
+
+        }
+
+        public void Calistir(string komutAdi, string path = "")
+        {
+            if (komutlar.ContainsKey(komutAdi)) 
+            {
+                if (komutlar[komutAdi] is Islem)
+                {
+                    ((Islem)komutlar[komutAdi]).Invoke();
+                }
+                else if (komutlar[komutAdi] is ParametrikIslem)
+                {
+                    ((ParametrikIslem)komutlar[komutAdi]).Invoke(path);
+                }
+                
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Beep();
+                Console.WriteLine($"'{komutAdi}', böyle bir komut bulunamadı.");
+                Console.ResetColor();
+            }
+        }
+    }
+    internal class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+
+            Islemler ıslemler = new Islemler();
+            ıslemler.EkranTemizlemek();
+
+            KomutYorumlayıcı interpreter = new KomutYorumlayıcı();
+
+            while (true)            
+            {
+                string klavyeGirdisi = Console.ReadLine();
+                string[] parcalar =  klavyeGirdisi.Split(' ');
+                if (parcalar.Length == 1)
+                {
+                    interpreter.Calistir(parcalar[0]);
+                }
+                else if(parcalar.Length == 2)
+                {
+                    interpreter.Calistir(parcalar[0], parcalar[1]);
+                }
+            }
+
+
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HashTableSamples-MyommandPrompt")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HashTableSamples-MyommandPrompt")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("30bd0740-9973-4388-8c65-68c9123ac5d4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
